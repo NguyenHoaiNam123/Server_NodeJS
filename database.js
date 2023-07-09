6b32bb42199b1638ae14f9cf603dbdbcb249c4f2
HOAI NAM NGUYEN <namloveheo123@gmail.com>
2023-07-10 02:16:30 +0700

    update

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const uri_compass = "mongodb://localhost:27017/englishapp"
+const uri_atlas = "mongodb+srv://namloveheo123:123456789Nam@cluster0.p8o1a0s.mongodb.net/englishapp?retryWrites=true&w=majority";
 async function connect() {
     try {
         await mongoose.set('strictQuery', true);
-        await mongoose.connect(uri_compass, {
+        await mongoose.connect(uri_atlas, {
             useNewUrlParser: true
         })
         console.log('connect db success')
